070e6504c3ec285d35cb8bd9873eee902e97d910
decrovert <decrovertp@gmail.com>
2022-03-26 19:26:13 +0000

    improving code in "decroghlangs.js"

--- decroghlangs.js
+++ decroghlangs.js
@@ -1,42 +1,51 @@
 window.onload = function() {
     const formsubmitbutton = document.getElementById("form_submit");
-
+    
     formsubmitbutton.onclick = async function() {
-        let username;
-        let repositoriesdata;
-        let repositoriesdata_json;
-        let local_languagesmap;
-        let local_languagesmap_json;
-        let global_languagesmap_bytes;
-        let global_languagesmap_percentage;
-        let total_bytes_of_code;
-        let currentlanguage_percentage;
+        const username = document.getElementById("form_ghusername")
+            .value;
+        
+        const repositoriesdata = await fetch(
+            "https://api.github.com/users/" + username + "/repos"
+        );
+            
+        const repositoriesdata_json = await repositoriesdata.json();
+            
         let chart;
-
-        username = document.getElementById("form_ghusername").value;
-        repositoriesdata = await fetch("https://api.github.com/users/" + username + "/repos");
-        repositoriesdata_json = await repositoriesdata.json();
-        global_languagesmap_bytes = new Map();
-        global_languagesmap_percentage = new Map();
-        total_bytes_of_code = 0;
-
+        
+        let global_languagesmap_bytes = new Map();
+        let global_languagesmap_percentage = new Map();
+        let total_bytes_of_code = 0;
+        
         for (const repository of repositoriesdata_json) {
-            local_languagesmap = await fetch(repository.languages_url);
-            local_languagesmap_json = await local_languagesmap.json();
-
-            for (const [language, written_bytes] of Object.entries(local_languagesmap_json)) {
+            let local_languagesmap = await fetch(repository.languages_url);
+            let local_languagesmap_json = await local_languagesmap.json();
+            
+            for (
+                const [language, written_bytes]
+                of Object.entries(local_languagesmap_json)
+            ) {
                 total_bytes_of_code += written_bytes;
-
+                    
                 if (global_languagesmap_bytes.has(language)) {
-                    global_languagesmap_bytes.set(language, global_languagesmap_bytes.get(language) + written_bytes);
+                    global_languagesmap_bytes.set(
+                        language,
+                        global_languagesmap_bytes.get(
+                            language
+                        ) + written_bytes
+                    );
+
                     continue;
                 }
-
+                            
                 global_languagesmap_bytes.set(language, written_bytes);
             }
         }
-
-        for (const [language, written_bytes] of global_languagesmap_bytes.entries()) {
+                    
+        for (
+            const [language, written_bytes]
+            of global_languagesmap_bytes.entries()
+        ) {
             /*
                 cross-multiplication
 
@@ -45,27 +54,39 @@ window.onload = function() {
 
                 x = 100 * written_bytes / total_bytes_of_code
             */
-            currentlanguage_percentage = 100 * written_bytes / total_bytes_of_code;
-            currentlanguage_percentage = Math.round(currentlanguage_percentage);
-            global_languagesmap_percentage.set(language, currentlanguage_percentage);
+
+            let currentlanguage_percentage =
+                100 * written_bytes / total_bytes_of_code;
+            
+            currentlanguage_percentage = Math.round(
+                currentlanguage_percentage
+            );
+            
+            global_languagesmap_percentage.set(
+                language,
+                currentlanguage_percentage
+            );
         }
 
         global_languagesmap_bytes.clear();
 
         chart = document.getElementById("toplangschart");
-
         chart.innerHTML = "";
         
-        for (const [language, percentage] of global_languagesmap_percentage.entries()) {
+        for (
+            const [language, percentage]
+            of global_languagesmap_percentage.entries()
+        ) {
             chart.innerHTML += "[";
 
             for (let i = 0; i < 10; ++i) {
-                chart.innerHTML += Math.round(percentage / 10) >= i ? "#" : "-";
+                chart.innerHTML += Math.round(percentage / 10) >= i
+                    ? "#"
+                    : "-";
             }
 
-            chart.innerHTML += "] " + percentage + "% " + language + "<br>";
+            chart.innerHTML +=
+                "] " + percentage + "% " + language + "<br>";
         }
-
-        console.log(global_languagesmap_percentage);
     };
 }
